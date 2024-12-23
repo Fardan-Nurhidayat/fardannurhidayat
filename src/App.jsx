a5265aa0bcf0fc43c0ca3d5315d6c9983c640c17
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2024-12-23 17:14:01 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+
+export default function App(){
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold">Hello, Tailwind!</h1>
+      <p className="text-lg text-gray-600">This is a simple Tailwind CSS example.</p>
+    </div>
+  )
+}
