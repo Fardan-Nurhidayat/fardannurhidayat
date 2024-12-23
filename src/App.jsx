e5edb805654a04e4769405dec87a46f64eec949a
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2024-12-23 22:52:25 +0700

    Chore : Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 
+import Header from './components/Header'
 
 export default function App(){
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold">Hello, Tailwind!</h1>
-      <p className="text-lg text-gray-600">This is a simple Tailwind CSS example.</p>
-    </div>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
