f4d76a53b4fd8050512e57f358904a661d1e6436
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2024-12-31 19:06:04 +0700

    ADd : skill section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
 export default function App() {
   return (
     <>
       <Header />
       <main>
         <Hero />
+        <About />
+        <Skills />
       </main>
     </>
   );
